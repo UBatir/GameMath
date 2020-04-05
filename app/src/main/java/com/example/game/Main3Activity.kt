71c993d40d47aacd0a3d918f896f80e8b89a0a53
UBatir <urazbaaev.br@gmail.com>
2020-04-05 12:31:07 +0500

    Added additional func

--- app/src/main/java/com/example/game/Main3Activity.kt
+++ app/src/main/java/com/example/game/Main3Activity.kt
@@ -12,8 +12,11 @@ class Main3Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
+        var rightAnswersCount=intent.getIntExtra(Main2Activity.RIGTH_ANSWERS_COUNT,11)
+        tvScore.text="$rightAnswersCount/${Main2Activity.level_Count}"
+        tvText.text=intent.getStringExtra("intent")
         val myanim:Animation=AnimationUtils.loadAnimation(this,R.anim.logo_anim)
-        tvWin.startAnimation(myanim)
+        tvText.startAnimation(myanim)
         btnGoTo.setOnClickListener {
             val intent=Intent(this,MainActivity::class.java)
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
